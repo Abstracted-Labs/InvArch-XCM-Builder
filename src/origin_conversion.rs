e61649134107963a4c15befc688b3f5df13b71aa
Gabriel Facco de Arruda <arrudagates@gmail.com>
2022-10-28 11:12:00 -0300

    feature gating tinkernet specific types

--- src/origin_conversion.rs
+++ src/origin_conversion.rs
@@ -1,49 +1,8 @@
 use crate::derivers::ParachainPluralityOriginDeriver;
-use core::{fmt::Debug, marker::PhantomData};
-use frame_support::{sp_runtime::traits::AtLeast32BitUnsigned, traits::OriginTrait};
-use frame_system::RawOrigin as SystemRawOrigin;
-use pallet_inv4::INV4Origin;
-use xcm::latest::{BodyId, BodyPart, Junction, Junctions, MultiLocation, NetworkId, OriginKind};
+use core::marker::PhantomData;
+use frame_support::traits::OriginTrait;
+use xcm::latest::{Junction, Junctions, MultiLocation, OriginKind};
 use xcm_executor::traits::ConvertOrigin;
-
-pub struct ConvertSignedOrMultisig<Origin, IpId, AccountId>(
-    core::marker::PhantomData<(Origin, IpId, AccountId)>,
-);
-
-impl<
-        Origin: OriginTrait + Clone + Debug,
-        IpId: AtLeast32BitUnsigned + Into<u32>,
-        AccountId: Into<[u8; 32]>,
-    > xcm_executor::traits::Convert<Origin, MultiLocation>
-    for ConvertSignedOrMultisig<Origin, IpId, AccountId>
-where
-    Origin::PalletsOrigin: From<SystemRawOrigin<AccountId>>
-        + TryInto<SystemRawOrigin<AccountId>, Error = Origin::PalletsOrigin>
-        + From<INV4Origin<IpId, AccountId>>
-        + TryInto<INV4Origin<IpId, AccountId>, Error = Origin::PalletsOrigin>,
-{
-    fn convert(o: Origin) -> Result<MultiLocation, Origin> {
-        let result = o.try_with_caller(|caller| match caller.clone().try_into() {
-            Ok(SystemRawOrigin::Signed(who)) => Ok(Junction::AccountId32 {
-                network: NetworkId::Any,
-                id: who.into(),
-            }
-            .into()),
-
-            _ => match caller.clone().try_into() {
-                Ok(INV4Origin::Multisig { id, .. }) => Ok(Junction::Plurality {
-                    id: BodyId::Index(id.into()),
-                    part: BodyPart::Voice,
-                }
-                .into()),
-
-                _ => Err(caller),
-            },
-        });
-
-        result
-    }
-}
 
 pub struct DeriveOriginFromPlurality<Origin, PluralityDeriver>(
     PhantomData<(Origin, PluralityDeriver)>,
@@ -73,6 +32,54 @@ where
                 },
             ) => PluralityDeriver::derive_account(para_id, id, part).ok_or(origin),
             (_, origin) => Err(origin),
+        }
+    }
+}
+
+#[cfg(feature = "for-tinkernet")]
+pub mod inv4_origin_conversion {
+    use core::fmt::Debug;
+    use frame_support::sp_runtime::traits::AtLeast32BitUnsigned;
+    use frame_system::RawOrigin as SystemRawOrigin;
+    use pallet_inv4::INV4Origin;
+    use xcm::latest::{BodyId, BodyPart, NetworkId};
+
+    pub struct ConvertSignedOrMultisig<Origin, IpId, AccountId>(
+        core::marker::PhantomData<(Origin, IpId, AccountId)>,
+    );
+
+    impl<
+            Origin: OriginTrait + Clone + Debug,
+            IpId: AtLeast32BitUnsigned + Into<u32>,
+            AccountId: Into<[u8; 32]>,
+        > xcm_executor::traits::Convert<Origin, MultiLocation>
+        for ConvertSignedOrMultisig<Origin, IpId, AccountId>
+    where
+        Origin::PalletsOrigin: From<SystemRawOrigin<AccountId>>
+            + TryInto<SystemRawOrigin<AccountId>, Error = Origin::PalletsOrigin>
+            + From<pallet_inv4::INV4Origin<IpId, AccountId>>
+            + TryInto<pallet_inv4::INV4Origin<IpId, AccountId>, Error = Origin::PalletsOrigin>,
+    {
+        fn convert(o: Origin) -> Result<MultiLocation, Origin> {
+            let result = o.try_with_caller(|caller| match caller.clone().try_into() {
+                Ok(SystemRawOrigin::Signed(who)) => Ok(Junction::AccountId32 {
+                    network: NetworkId::Any,
+                    id: who.into(),
+                }
+                .into()),
+
+                _ => match caller.clone().try_into() {
+                    Ok(pallet_inv4::INV4Origin::Multisig { id, .. }) => Ok(Junction::Plurality {
+                        id: BodyId::Index(id.into()),
+                        part: BodyPart::Voice,
+                    }
+                    .into()),
+
+                    _ => Err(caller),
+                },
+            });
+
+            result
         }
     }
 }
